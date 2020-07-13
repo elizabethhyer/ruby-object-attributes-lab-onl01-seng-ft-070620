5ed5b5573e9a45bfc7faaf39421f01a65ac94e77
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-13 15:39:13 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+ 
+  def name
+    @name
+  end
+ 
+  def name=(new_name)
+    @name = new_name
+  end
+   
+  def initialize(breed)
+    @breed = breed
+  end 
+  
+  def breed
+    @breed
+  end 
+  
+  def breed=(new_breed)
+    @job = new_breed
+end
+end 
+
